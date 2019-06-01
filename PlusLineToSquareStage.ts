1932978f325d066c7c2afbb8bb5631c4d6b9fe76
Anwesh43 <anweshthecool0@gmail.com>
2019-06-01 12:38:44 +0530

    adding constants

---
+++ PlusLineToSquareStage.ts
@@ -0,0 +1,11 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const nodes : number = 5
+const lines : number = 4
+const deg : number = Math.PI / 4
+const foreColor : string = "#311B92"
+const backColor : string = "#BDBDBD"
